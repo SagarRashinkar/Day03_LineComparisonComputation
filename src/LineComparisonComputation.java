e64f4635cc43f8c1c3b3e45d88ec86ea4076c6c3
Sagar Rashinkar <sagarrashinkar2@gmail.com>
2022-08-15 18:53:57 +0530

    Added: Line comparison program

---
+++ src/LineComparisonComputation.java
@@ -0,0 +1,11 @@
+public class LineComparisonComputation {
+
+    public void welcomeMessage(){
+        System.out.println("Welcome to Line Comparison Computation Program!!!");
+    }
+
+    public static void main(String[] args) {
+        LineComparisonComputation obj = new LineComparisonComputation();
+        obj.welcomeMessage();
+    }
+}
